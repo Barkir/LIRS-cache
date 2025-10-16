f501d60115b3597c73b17579c9f9445d0d9319ec
Barkir <r.barinov2006@gmail.com>
2025-10-17 01:19:52 +0300

    Started adding vizualization library...

--- include/vizualizer.hpp
+++ include/vizualizer.hpp
@@ -1,5 +1,27 @@
+#include <functional>
+
 template <typename cache_T, typename KeyT = int>
 class VizCache {
+    private:
+        using getFuncT = std::function<T (const KeyT&)>;
+        using insertFuncT = std::function<T (const KeyT&, T&)>;
+
+        getFuncT getFunc_;
+        insertFuncT insertFunc_;
+
     public:
-        bool get()
+        VizCache(getFuncT getFunc, insertFuncT insertFunc) : getFunc_(getFunc), insertFuncT_(insertFunc) {}
+    public:
+        bool get(const KeyT& key) {
+            T elem = getFunc_(key);
+
+            /* update vizualization */
+
+        }
+
+        bool insert(const KeyT& key, T& elem) {
+            insertFunc_(key, elem);
+
+            /* update vizualization */
+        }
 }
